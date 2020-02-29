7cb99ff5ec705ca594133af68af2933923e96c70
Shrey Limbani <shreylimbani@gmail.com>
2020-02-29 13:54:02 +0530

    Added Modal

--- newbooking.php
+++ newbooking.php
@@ -96,6 +96,40 @@ if (isset($_POST['book'])) {
 
 <body>
 
+<!-- Central Modal Medium Success -->
+<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModal"
+  aria-hidden="true">
+  <div class="modal-dialog modal-notify modal-success" role="document">
+    <!--Content-->
+    <div class="modal-content">
+      <!--Header-->
+      <div class="modal-header">
+        <p class="heading lead">Ticket Confirmation</p>
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true" class="white-text">&times;</span>
+        </button>
+      </div>
+
+      <!--Body-->
+      <div class="modal-body">
+        <div class="text-center">
+          <i class="fas fa-check fa-4x mb-3 animated rotateIn"></i>
+          <p>Total ticket .</p>
+        </div>
+      </div>
+
+      <!--Footer-->
+      <div class="modal-footer justify-content-center">
+        <a type="button" class="btn btn-success">Get it now <i class="far fa-gem ml-1 text-white"></i></a>
+        <a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">No, thanks</a>
+      </div>
+    </div>
+    <!--/.Content-->
+  </div>
+</div>
+<!-- Central Modal Medium Success-->
+
   <!-- Navbar -->
   <nav class="navbar  navbar-expand-lg navbar-light white scrolling-navbar">
     <div class="container">
